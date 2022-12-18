87c025245df5ae73bb8d857d55fb86b65df69c3a
cutratefuture <christian@cutratefuture.com>
2022-12-17 19:48:35 -0800

    commit changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+});
